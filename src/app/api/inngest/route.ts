39a2d5ba11b99d6cc58da702644908d1c8022997
paarth293 <i.m.paarthgupta@gmail.com>
2026-09-04 17:25:27 +0530

    registered publishAuditAnchor in inngest route handler

--- src/app/api/inngest/route.ts
+++ src/app/api/inngest/route.ts
@@ -2,11 +2,12 @@ import { serve } from "inngest/next";
 import { inngest } from "@/server/inngest/client";
 import {
   generateAuditLog,
+  publishAuditAnchor,
   reconcileStaleOrders,
   recoverFailedPayment,
 } from "@/server/inngest/functions";
 
 export const { GET, POST, PUT } = serve({
   client: inngest,
-  functions: [recoverFailedPayment, generateAuditLog, reconcileStaleOrders],
+  functions: [recoverFailedPayment, generateAuditLog, reconcileStaleOrders, publishAuditAnchor],
 });
